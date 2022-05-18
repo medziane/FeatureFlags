be59193653d4be60a5a2e4a60106821b4f571485
MED ZIANE <med.ziane@gmail.com>
2022-05-18 02:08:47 +0200

    Add EnabledWhenAnyDisabledFeatureFlag

--- src/FeatureFlags/EnabledWhenAnyDisabledFeatureFlag.cs
+++ src/FeatureFlags/EnabledWhenAnyDisabledFeatureFlag.cs
@@ -1,17 +1,17 @@
 namespace Med.FeatureFlags;
 
 /// <summary>
-/// A feature flag that is enabled when any of the given feature flags is enabled.
+/// A feature flag that is enabled when any of the given feature flags is disabled.
 /// </summary>
-public class EnabledWhenAnyEnabledFeatureFlag :
+public class EnabledWhenAnyDisabledFeatureFlag :
     FeatureFlag
 {
     /// <summary>
-    /// Creates a new instance of the <see cref="EnabledWhenAnyEnabledFeatureFlag"/> class.
+    /// Creates a new instance of the <see cref="EnabledWhenAnyDisabledFeatureFlag"/> class.
     /// </summary>
     /// <param name="featureFlags">A set of feature flags.</param>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="featureFlags"/> is <c>null</c>.</exception>
-    public EnabledWhenAnyEnabledFeatureFlag(params IFeatureFlag[] featureFlags)
+    public EnabledWhenAnyDisabledFeatureFlag(params IFeatureFlag[] featureFlags)
         : base(() => EvaluateEnabled(featureFlags))
     {
         FeatureFlags = featureFlags ?? throw new ArgumentNullException(nameof(featureFlags));
@@ -21,12 +21,12 @@ public class EnabledWhenAnyEnabledFeatureFlag :
     }
 
     /// <summary>
-    /// Creates a new instance of the <see cref="EnabledWhenAnyEnabledFeatureFlag"/> class.
+    /// Creates a new instance of the <see cref="EnabledWhenAnyDisabledFeatureFlag"/> class.
     /// </summary>
     /// <param name="id">An identifier for this feature flag instance.</param>
     /// <param name="featureFlags">A set of feature flags.</param>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="featureFlags"/> is <c>null</c>.</exception>
-    public EnabledWhenAnyEnabledFeatureFlag(string id, params IFeatureFlag[] featureFlags)
+    public EnabledWhenAnyDisabledFeatureFlag(string id, params IFeatureFlag[] featureFlags)
         : base(id, () => EvaluateEnabled(featureFlags))
     {
         FeatureFlags = featureFlags ?? throw new ArgumentNullException(nameof(featureFlags));
@@ -44,10 +44,10 @@ public class EnabledWhenAnyEnabledFeatureFlag :
     /// Evaluates whether or not this feature flag is enabled.
     /// </summary>
     /// <param name="featureFlags">The set of feature flags that contribute to the state of this feature flag.</param>
-    /// <returns><c>true</c> if any of the <paramref name="featureFlags"/> is enabled. Otherwise, <c>false</c>.</returns>
+    /// <returns><c>true</c> if any of the <paramref name="featureFlags"/> is disabled. Otherwise, <c>false</c>.</returns>
     protected static bool EvaluateEnabled(IEnumerable<IFeatureFlag> featureFlags)
     {
-        return featureFlags.Any(x => x.Enabled);
+        return featureFlags.Any(x => !x.Enabled);
     }
 
     /// <summary>
