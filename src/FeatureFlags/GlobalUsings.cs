0cf55d447ba777981768a9e32a3c4b7262a7cbcc
MED ZIANE <med.ziane@gmail.com>
2022-05-28 01:58:32 +0200

    Add to GlobalUsings

--- src/FeatureFlags/GlobalUsings.cs
+++ src/FeatureFlags/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using System;
+global using System.Collections;
 global using System.ComponentModel;
+global using System.ComponentModel.DataAnnotations;
 global using System.Runtime.CompilerServices;
