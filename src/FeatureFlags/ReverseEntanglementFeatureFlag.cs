5a3009ae39b75e500d7b0d4979414c8e464f74c2
MED ZIANE <med.ziane@gmail.com>
2022-05-28 01:59:42 +0200

    Rename ReverseEnabledFeatureFlag to ReverseEntanglementFeatureFlag

--- src/FeatureFlags/ReverseEntanglementFeatureFlag.cs
+++ src/FeatureFlags/ReverseEntanglementFeatureFlag.cs
@@ -3,24 +3,24 @@ namespace Med.FeatureFlags;
 /// <summary>
 ///     A feature flag that is enabled when the given feature flag is disabled, and vice versa.
 /// </summary>
-public class ReverseEnabledFeatureFlag :
+public class ReverseEntanglementFeatureFlag :
     EnabledWhenAllDisabledFeatureFlag
 {
     /// <summary>
-    ///     Creates a new instance of the <see cref="ReverseEnabledFeatureFlag" /> class.
+    ///     Creates a new instance of the <see cref="ReverseEntanglementFeatureFlag" /> class.
     /// </summary>
     /// <param name="featureFlag">The feature flag to be opposed by this one.</param>
-    public ReverseEnabledFeatureFlag(IFeatureFlag featureFlag)
+    public ReverseEntanglementFeatureFlag(IFeatureFlag featureFlag)
         : base(featureFlag)
     {
     }
 
     /// <summary>
-    ///     Creates a new instance of the <see cref="ReverseEnabledFeatureFlag" /> class.
+    ///     Creates a new instance of the <see cref="ReverseEntanglementFeatureFlag" /> class.
     /// </summary>
     /// <param name="id">An identifier for this feature flag instance.</param>
     /// <param name="featureFlag">The feature flag to be opposed by this one.</param>
-    public ReverseEnabledFeatureFlag(string id, IFeatureFlag featureFlag)
+    public ReverseEntanglementFeatureFlag(string id, IFeatureFlag featureFlag)
         : base(id, featureFlag)
     {
     }
