8f4de04a90c516ccd78a8ec4977910a0f5d183c8
MED ZIANE <med.ziane@gmail.com>
2022-05-28 01:59:10 +0200

    Rename InSyncEnabledFeatureFlag to EntanglementFeatureFlag

--- src/FeatureFlags/EntanglementFeatureFlag.cs
+++ src/FeatureFlags/EntanglementFeatureFlag.cs
@@ -3,24 +3,24 @@ namespace Med.FeatureFlags;
 /// <summary>
 ///     A feature flag that is enabled when the given feature flag is also enabled, and vice versa.
 /// </summary>
-public class InSyncEnabledFeatureFlag :
+public class EntanglementFeatureFlag :
     EnabledWhenAllEnabledFeatureFlag
 {
     /// <summary>
-    ///     Creates a new instance of the <see cref="InSyncEnabledFeatureFlag" /> class.
+    ///     Creates a new instance of the <see cref="EntanglementFeatureFlag" /> class.
     /// </summary>
     /// <param name="featureFlag">The feature flag, the state of which is to be shared by this one.</param>
-    public InSyncEnabledFeatureFlag(IFeatureFlag featureFlag)
+    public EntanglementFeatureFlag(IFeatureFlag featureFlag)
         : base(featureFlag)
     {
     }
 
     /// <summary>
-    ///     Creates a new instance of the <see cref="InSyncEnabledFeatureFlag" /> class.
+    ///     Creates a new instance of the <see cref="EntanglementFeatureFlag" /> class.
     /// </summary>
     /// <param name="id">An identifier for this feature flag instance.</param>
     /// <param name="featureFlag">The feature flag, the state of which is to be shared by this one.</param>
-    public InSyncEnabledFeatureFlag(string id, IFeatureFlag featureFlag)
+    public EntanglementFeatureFlag(string id, IFeatureFlag featureFlag)
         : base(id, featureFlag)
     {
     }
